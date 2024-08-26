952647bcf48749711eb44da491ab9c5d1dbbc86a
Your Name <you@example.com>
2024-08-26 20:51:56 +0200

    Added fzf to path

--- roles/bash/files/bash/paths_vars.sh
+++ roles/bash/files/bash/paths_vars.sh
@@ -2,9 +2,8 @@
 
 addToPath /usr/local/go/bin
 addToPath $GOPATH/bin
-addToPath $HOME/go/bin
 addToPath $HOME/.dotfiles/bin
-addToPath $HOME/.cargo/bin
+addToPath $HOME/.fzf/bin
 addToPathFront $HOME/.local/bin
 # ccache
 addToPathFront /usr/lib/ccache

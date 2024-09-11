52af83ddfaac04ec6a8b4fc9cf25dae19eb31450
MikkelFilip <mikkelfilip1@gmail.com>
2024-09-11 10:35:15 +0200

    updated aliases for ls

--- roles/bash/files/bash/misc_aliases.sh
+++ roles/bash/files/bash/misc_aliases.sh
@@ -5,6 +5,7 @@ alias comment-header='toilet -f pagga -S'
 alias i='explorer.exe'
 alias ncdu='ncdu --color dark -e -q --exclude-caches --exclude-kernfs -L'
 alias which='alias | /usr/bin/which --tty-only --read-alias --show-dot --show-tilde'
-alias ls="eza"
-alias ll='eza -l -h --git'
-alias la='eza -la -h --git'
+alias ls="eza --icons"
+alias ll='eza -l -h --git --icons'
+alias la='eza -la -h --git --icons'
+
